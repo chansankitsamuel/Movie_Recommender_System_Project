785cc21829635d60ed95bbfc90c3dd43fc4fb726
chansankitsamuel <chansankitsamuel@gmail.com>
2026-01-13 22:34:02 +0800

    Found dataset, working on the data processing

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
-from fastapi import FastAPI
+import os
+import ast
 
-app = FastAPI()
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-@app.get("/")
-def root():
-    return {"Hello": "World"}
+
+# read data from csv
+movies = pd.read_csv("data/tmdb_5000_movies.csv")
+credits = pd.read_csv("data/tmdb_5000_credits.csv")
+
+# merge 2 tables and simplify it
+credits.rename(columns={'movie_id':'id'}, inplace=True)
+data = pd.merge(movies, credits, on=["id", "title"])
+data = data[["id", "title", "overview", "genres", "keywords", "cast", "crew"]]
+data.dropna(inplace=True)
+
+# convert columns into readable format
+def convert(s): 
+    l = []
+    for i in ast.literal_eval(s):
+        l.append(i["name"])
+    return l
+data["genres"] = data["genres"].apply(convert)
+data["keywords"] = data["keywords"].apply(convert)
+data["cast"] = data["cast"].apply(convert)
+def fetch_director(s):
+    l = []
+    for i in ast.literal_eval(s):
+        if i["job"] == "Director":
+            return i["name"]
+data["crew"] = data["crew"].apply(fetch_director)
+data.rename(columns={'crew':'director'}, inplace=True)
+data["overview"] = data["overview"].apply(lambda s:s.split())
+
+
+# debug zone
+print(data.head(1)['overview'])
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Project Objective
+Create a content-based recommendation system for movies
+
+
+# Sources / References
+
+- Dataset: https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata
+
